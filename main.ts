7a1865213d21c15f1a7c408a95825e2d3413e9e5
VioletteZ <115679845+violettezskz@users.noreply.github.com>
2022-11-04 10:58:53 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,32 @@
+let x = 0
+let y = 0
+input.onButtonPressed(Button.A, function () {
+    x = 0
+    y = 0
+    for (let index = 0; index < 5; index++) {
+        for (let index = 0; index < 5; index++) {
+            led.plot(x, y)
+            basic.pause(100)
+            x += 1
+        }
+        y += 1
+        x = 0
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    x = 0
+    y = 0
+    for (let index = 0; index < 4; index++) {
+        for (let index = 0; index < 5; index++) {
+            led.plot(x, y)
+            basic.pause(100)
+            x += 1
+        }
+        y += 1
+        for (let index = 0; index < 5; index++) {
+            x += -1
+            led.plot(x, y)
+            basic.pause(100)
+        }
+    }
+})
